db2b25e49ddd2643d8d87ec721ed81895d1729bf
lisajoanno <lisa.joanno@gmail.com>
2016-04-07 15:39:12 +0200

    Cassage de exception

--- app/exceptions/src/main/java/fr/unice/polytech/isa/exceptions/AlreadyExistingCustomerException.java
+++ app/exceptions/src/main/java/fr/unice/polytech/isa/exceptions/AlreadyExistingCustomerException.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 
 public class AlreadyExistingCustomerException extends Exception implements Serializable {
-
+CASSAGE
 	private String conflictingName;
 
 	public AlreadyExistingCustomerException(String name) {
